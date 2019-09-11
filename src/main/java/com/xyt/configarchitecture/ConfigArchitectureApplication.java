d95b18cadc9667c8b55cb96ae823ec5f27506534
cluo_glb <rollex_glb@test.com>
2019-09-11 09:53:52 +0800

    刷新

--- src/main/java/com/xyt/configarchitecture/ConfigArchitectureApplication.java
+++ src/main/java/com/xyt/configarchitecture/ConfigArchitectureApplication.java
@@ -3,11 +3,13 @@ package com.xyt.configarchitecture;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
+@RefreshScope
 public class ConfigArchitectureApplication {
 
     public static void main(String[] args) {
